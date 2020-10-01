194b4eccf985c1d23c85d0c8421a33c100a27967
abhijitj <abhijitj@mkcl.org>
2020-10-02 00:44:54 +0530

    first commit

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
-# README for Pro-act
+## Features
 
-This is the README for extension "Pro-act". For better handling project files
+This extension allows to show alert when opening or save the edted file.
 
-Handling the common important project files by effective way of notification. 
+you can add message and Contact person name to show information before change the file
 
-Extension will show the handler of perticuler file while changing or modifying its contents.
+if a file contains a comment with text like:
 
-Contributers : 
+~~MSG: This is informative message ~~CONTACT: HARRY 
 
-1] Abhijeet Jirole - abhijirole123@gmail.com
 
-2] AKash WaDhawane - akash.wadhawane@gmail.com
+This message will how when open the file with contact person name. or after save the edited file Its show only infomative message.
+~~MSG: keyword important when you want to show message and ~~CONTACT: is important when you want to show contact person name. 
+you can able to add only message if you want but not contact person name. 
+
+
+Examples:
+
+// ~~MSG: info message here 
+
+//~~MSG: info message here  ~~CONTACT: contact person name
+
+
+
+### For more information
+
+- [Github Repository](https://github.com/abhijirole/Pro-act)
+
 
 **Enjoy!**
-
-
--- extension.js
+++ extension.js
@@ -3,130 +3,80 @@ const { textChangeRangeIsUnchanged, Extension } = require('typescript');
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const fs = require("fs");
-// ##ERROR:ALWAYS This is a test error message that will be shown to anyone who opens the file.
-// this method is called when your extension is activated
+
+/* ##STATUSBAR:EDIT:ALWAYS statusbar message here */
+
 
 // your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+function activate (context) {
 
-	
+
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "demo" is now active!');
+	// console.log('Congratulations, your extension "demo" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('demo.helloWorld',async function () {
+	let disposable = vscode.commands.registerCommand('demo.helloWorld', async function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Welcome to AJs Extension!');
-		////1 START
-		// vscode.window.showInputBox()
-		// const act=vscode.window.activeTextEditor
-		// if(!act){
-		// 	vscode.window.showErrorMessage("not active")
-		// 	return
-		// }
-		
-		// const text=act.document.getText(act.selection)
-		// vscode.window.showInformationMessage(`"user text:-", ${text}`)
-		//// 1 END
+		// vscode.window.showInformationMessage('Welcome to AJs Extension!');
 
 
-		////2 START
-		// vscode.workspace.onDidChangeTextDocument(event=>{
-		// 	console.log("eafasfasdfsdfasd",event, event.document.fileName)
-		// })
-		 
-		const files =await vscode.workspace.findFiles(
-			"**/abc.js*",
-			"**/node_modules/**"
-		  );
 
-		  console.log("Files", files)
-		var filepath=""
-		  files.forEach((file) => {
-			fs.readFile(file.fsPath, function read(err, data) {
-			  if (err) {
-				throw err;
-			  }
-			  const content = data;
-			  console.log("content", content.toString("utf8").split("\n"))
-			//   processFile(content);
-			var file= content.toString("utf8").split("\n")
-			filepath=file[0]
-			});
-	
-			// if (errOccuredCount > 0) {
-			//   vscode.window.showInformationMessage(
-			// 	"I am still in devolopement mode :( some repos not downloaded ErrCount:" +
-			// 	errOccuredCount
-			//   );
-			// } else {
-			//   vscode.window.showInformationMessage(
-			// 	"Github Repos Scanned Succesfully ! "
-			//   );
-			// }
-		  });
+		vscode.workspace.onDidOpenTextDocument(e => showMessages(e))
+		vscode.workspace.onDidSaveTextDocument(e => showMessages2(e))
 
-
-		  vscode.workspace.onDidOpenTextDocument(event=>{
-			  console.log("Open DOcument", event)
-				vscode.window.showWarningMessage("Please cofirm your changes ");
-			  
-
-		  })
-		  await vscode.workspace.onDidSaveTextDocument(event2=>{
-			// console.log("save event for file", event2.fileName, filepath)
-			var name=filepath.replace(/(")/, ' ')
-			var name2=name.replace(/(")/, ' ')
-			var fileName2=event2.fileName.toString()
-			console.log(typeof( fileName2.toLocaleLowerCase()), typeof(name2.toLocaleLowerCase()))
-			console.log(  name2.toLocaleLowerCase(), fileName2.toLocaleLowerCase())
-			// if(event2.fileName.toString().toLocaleLowerCase()==name2.toLocaleLowerCase())	{
-			// 	console.log("called")
-			// 	 vscode.window.showInformationMessage('Take a permission before change to this file');
-			// }
-			if (name2.toLocaleLowerCase().includes(fileName2.toLocaleLowerCase())===true){
-				vscode.window.showWarningMessage("This file added as a secure file please verify before merge ");
-				// vscode.window.showOpenDialog("abc":"asd":"/asdasd")
-				// vscode.window.showInformationMessage(`'File path:-', ${name2}`);
-				vscode.window.showSaveDialog()
-				console.log("Yes GOt it")
-			}else{
-				
-				console.log("Yes NOT GOt it")
-			}
-		})
-
-		////2 END
-
-		// 3 NOT Working Yet
-		const Quickpack=vscode.window.createQuickPick()
-
-		Quickpack.onDidChangeSelection(event=>{
-			console.log("called",)
-			if(event){
-
-				vscode.window.showInformationMessage('please dont change anything');
-			}
-			vscode.window.showInformationMessage('please dont change anythin22g');
-		})	
 	});
 
 	context.subscriptions.push(disposable);
 }
-exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate () { }
+
+function showMessages (event) {
+	var text = event.getText()
+	// console.log("after open", text)
+	let re = new RegExp('(--|//|/\*)\\s*~~(MSG):?\\s+(.*)');
+	let match
+	match = re.exec(text)
+	if (match != null) {
+		let str = match[0].substr(match[0].indexOf(":") + 1)
+		let firstMSg = str.split("~~")[0]
+		let re2 = new RegExp('(--|//|/\*)\\s*~~(CONTACT):?\\s+(.*)');
+		if (re2.exec(match[0]) != null) {
+
+			vscode.window.showErrorMessage(firstMSg + '\r\n' + "CONTACT PERSON:-" + re2.exec(match[0])[3], { modal: true })
+		} else {
+			vscode.window.showErrorMessage(firstMSg + '\r\n', { modal: true })
+
+		}
+	}
+
+}
+function showMessages2 (event) {
+	var text = event.getText()
+	// console.log("after open", text)
+	let re = new RegExp('(--|//|/\*)\\s*~~(MSG):?\\s+(.*)');
+	let match
+	match = re.exec(text)
+	if (match != null) {
+		let str = match[0].substr(match[0].indexOf(":") + 1)
+		let firstMSg = str.split("~~")[0]
+		vscode.window.showInformationMessage(firstMSg + '\r\n')
+
+	}
+
+}
+exports.activate = activate;
 
 module.exports = {
 	activate,
